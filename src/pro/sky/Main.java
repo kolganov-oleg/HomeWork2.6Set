fce2036a74d836c2b170dc33ab4a92c278b3da52
kolgan <6775366@gmail.com>
2022-10-10 03:24:49 +0300

    Initial commit

---
+++ src/pro/sky/Main.java
@@ -0,0 +1,14 @@
+package pro.sky;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> words = new ArrayList<>(List.of("a", "b", "c", "ab", "b", "c"));
+
+    public static void main(String[] args) {
+
+    }
+}
